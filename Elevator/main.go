2ec2e8141df6f9e069404f58145b4750de6b0d49
Tora Grenness Haga <toragh@stud.ntnu.no>
2018-03-05 13:37:16 +0100

    merged master to netwrok and status branch

--- Elevator/main.go
+++ Elevator/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"./Driver/Elevio"
 	//"./Status"
 	"./Network"
-=======
-	//"./Driver/Elevio"
-	"./Fsm"
->>>>>>> 3cfb9cd8b8d0cdca86521ef3e52f4d33a61b1e74
+
+
 )
 
 type UpdateMsg struct{
@@ -27,7 +24,6 @@ FLOORS := 4
 ELEVATORS := 3
 
 func main() {
-<<<<<<< HEAD
 
 
 	InternalUpdate := make(chan UpdateMsg) //sends updates that occured in this node to the network module
@@ -37,12 +33,9 @@ func main() {
 	go network.Network(InternalUpdate, ExternalUpdate)
 	go status.Status(InternalUpdate, ExternalUpdate)
 
-
-=======
 	//numFloors := 4
 
 	//elevio.Init("localhost:15657", numFloors)
 	fsm.CalculateOptimalElevator("test")
 
->>>>>>> 3cfb9cd8b8d0cdca86521ef3e52f4d33a61b1e74
 }
